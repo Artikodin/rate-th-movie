25b05072da1db70fb4e01e77b769110bc43b39b7
Artium Wasse <artium.wasse@vimcar.com>
2022-01-30 18:25:39 +0100

    feat(cats): Add cats services

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -8,32 +8,21 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cats.interface';
 import { CatDto } from './cats.dto';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
   create(@Body() createCatDto: CatDto) {
-    return 'This action adds a new cat';
+    this.catsService.create(createCatDto);
   }
 
   @Get()
-  findAll(@Query('limit') limit: string) {
-    return `This action returns all cats (limit: ${limit} items)`;
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `This action returns a #${id} cat`;
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateCatDto: CatDto) {
-    return `This action updates a #${id} cat`;
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `This action removes a #${id} cat`;
+  findAll(): Cat[] {
+    return this.catsService.findAll();
   }
 }
